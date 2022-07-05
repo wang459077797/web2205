981e33533bf016db8d453bdcd35ec1e44deb3a8f
LL <459077797@qq.com>
2022-07-05 14:52:49 +0800

    up

---
+++ web2/firstjs.js
@@ -0,0 +1,21 @@
+// var username = '张三'
+// var str3 = `hello ${username}`
+// console.log(str3.length)
+// console.log(str3.charAt(6));
+// console.log(str3.charAt(7));
+
+// var str4 = '张三长得"无敌帅"'
+// var str5 = "丽丽是个'学神'"
+// var str3 = '我是'+'超级无敌美少女'+'加'+'奥特曼'+"变身"
+
+// console.log( str4.lastIndexOf("敌123")) //获取指定字符串的首次出现的下标位置， 如果没找到返回-1
+// console.log(str4.replace("123213","巨丑"))
+// // console.log(str4.split("\"")) 
+// console.log(str4.substr(5,2)) //截取字符串 // 下标 长度
+// console.log(str4.substring(5,7)) //截取字符串);  //下标 下标
+// var str = 'abcEFaGa'
+// console.log(str.toUpperCase()) //转换为大写);
+// console.log(str.toLowerCase()) //转换为小写);
+// console.log(str.repeat(5)) //复制字符串次数
+ 
+// console.log(str.replaceAll('a','A'));//替换字符串
